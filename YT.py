53cba73b728d24b409f8c3969f1ecdaad31a04cf
Avi Gupta <41137189+Avihwr@users.noreply.github.com>
2020-09-17 20:22:31 +0530

    Add files via upload

---
+++ YT.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+
+link = str(input("Enter the URL of the Youtube Video: "))
+Timer = int(input("Enter the number of seconds you want: "))
+
+views = int(input('Enter the number of views you want: '))
+
+driver = webdriver.Chrome()
+driver.get(link)
+
+for i in range(views):
+    time.sleep(Timer)
+    driver.refresh()
+    print(i)
+    exit()
+
