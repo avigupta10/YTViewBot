f64b9042709cad0b7d0bf00f346354843f884dd8
Avi Gupta <41137189+Avihwr@users.noreply.github.com>
2020-09-18 19:29:57 +0530

    Update YT.py

--- YT.py
+++ YT.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 
 link = str(input("Enter the URL of the Youtube Video: "))
-Timer = int(input("Enter the number of seconds you want: "))
+Timer = int(input("Enter the time delay you want: "))
 
 views = int(input('Enter the number of views you want: '))
 
